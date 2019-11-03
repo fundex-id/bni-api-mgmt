e8db1b18a7953f78d0a900e7f7305ec55f81c63d
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-04 05:47:51 +0700

    BNIOperation

---
+++ operation.go
@@ -0,0 +1,14 @@
+package bni
+
+const (
+	InHouseInquiryRequest = "IN_HOUSE_INQUIRY_REQUEST"
+	InHouseInquiryResponse = "IN_HOUSE_INQUIRY_RESPONSE"
+	InterBankInquiryRequest = "INTER_BANK_INQUIRY_REQUEST"
+	InterBankInquiryResponse = "INTER_BANK_INQUIRY_RESPONSE"
+	FeeTransferRequest = "FEE_TRANSFER_REQUEST"
+	FeeTransferResponse = "FEE_TRANSFER_RESPONSE"
+	InHouseTransferRequest = "IN_HOUSE_TRANSFER_REQUEST"
+	InHouseTransferResponse = "IN_HOUSE_TRANSFER_RESPONSE"
+	InterBankTransferRequest = "INTER_BANK_TRANSFER_REQUEST"
+	InterBankTransferResponse = "INTER_BANK_TRANSFER_RESPONSE"
+)
