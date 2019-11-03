d81dd4fb764216e10e24272819337147926df3f1
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-03 11:39:58 +0700

    signature draft

--- signature.go
+++ signature.go
@@ -1,5 +1,19 @@
 package bni
 
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"io/ioutil"
+
+	"github.com/fundex-id/bni-api-mgmt/logger"
+	"github.com/juju/errors"
+)
+
 type Signature struct {
 	config SignatureConfig
 }
@@ -10,4 +24,44 @@ func newSignature(config SignatureConfig) *Signature {
 
 func (s *Signature) sha256WithRsa(data string) (string, error) {
 
+	privateKey, err := loadPrivateKeyFromPEMFile(s.config.PrivateKeyPath)
+	if err != nil {
+		return "", errors.Trace(err)
+	}
+
+	h := sha256.New()
+	h.Write([]byte(data))
+	d := h.Sum(nil)
+
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, d)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Infof("Signature in byte: %v\n\n", signature)
+
+	encodedSig := base64.StdEncoding.EncodeToString(signature)
+
+	logger.Infof("Encoded signature: %v\n\n", encodedSig)
+
+	return encodedSig, nil
+}
+
+func loadPrivateKeyFromPEMFile(privKeyFileLocation string) (*rsa.PrivateKey, error) {
+	fileData, err := ioutil.ReadFile(privKeyFileLocation)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	block, _ := pem.Decode(fileData)
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return nil, errors.New("Failed to load a valid private key")
+	}
+
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	return privateKey, nil
 }
