53de9975e64d74fada48fa8b4f5077f53cd12f04
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-05 13:09:30 +0700

    integration test of DoAuthentication

---
+++ bni_test.go
@@ -0,0 +1,90 @@
+package bni
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	bniCtx "github.com/fundex-id/bni-api-mgmt/context"
+	"github.com/fundex-id/bni-api-mgmt/dto"
+	"github.com/fundex-id/bni-api-mgmt/util"
+	"github.com/pborman/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBNI_DoAuthentication(t *testing.T) {
+	t.Run("good case", func(t *testing.T) {
+		givenConfig := Config{
+			AuthPath: "/oauth",
+			Username: "dummyusername",
+			Password: "dummypassword",
+		}
+
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			assert.Equal(t, req.Method, http.MethodPost)
+			assert.Equal(t, req.URL.String(), givenConfig.AuthPath)
+
+			assert.Equal(t, req.Header.Get("content-type"), "application/x-www-form-urlencoded")
+			assert.Equal(t, req.Header.Get("authorization"), "Basic "+basicAuth(givenConfig.Username, givenConfig.Password))
+
+			var dtoResp dto.GetTokenResponse
+			getJSON("testdata/get_token_response.json", &dtoResp)
+
+			w.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(w).Encode(dtoResp)
+			util.AssertErrNil(t, err)
+
+		}))
+		defer testServer.Close()
+
+		givenConfig.BNIServer = testServer.URL
+
+		bni := New(givenConfig)
+		bni.api.httpClient = testServer.Client()
+
+		ctx := bniCtx.WithReqId(context.Background(), uuid.NewRandom().String())
+		dtoResp, err := bni.DoAuthentication(ctx)
+		util.AssertErrNil(t, err)
+		if assert.NotNil(t, dtoResp) {
+			assert.NotEmpty(t, dtoResp.Session.ID)
+		}
+
+	})
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func getJSON(filePath string, obj interface{}) {
+	absPath, err := filepath.Abs(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open(absPath)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(byteValue, &obj)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
