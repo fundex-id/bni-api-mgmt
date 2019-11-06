2029fb6d80a6f0af8b8294c10c30d44c55126d88
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-06 08:48:18 +0700

    uti: AssertErrNil & AssertErrNotNil return err existence

--- util/util.go
+++ util/util.go
@@ -2,16 +2,20 @@ package util
 
 import "testing"
 
-func AssertErrNil(t *testing.T, err error) {
+func AssertErrNil(t *testing.T, err error) bool {
 	t.Helper()
 	if err != nil {
 		t.Errorf("Expect nil, but got: %+v", err)
 	}
+
+	return err == nil
 }
 
-func AssertErrNotNil(t *testing.T, err error) {
+func AssertErrNotNil(t *testing.T, err error) bool {
 	t.Helper()
 	if err == nil {
 		t.Errorf("Expect not nil, but got: %+v", err)
 	}
+
+	return err != nil
 }
