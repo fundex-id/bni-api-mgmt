cee33de53ba6d80d03c9ef7cc714467542456c0c
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-07 06:21:31 +0700

    operation: BalanceRequest & BalanceResponse

--- operation.go
+++ operation.go
@@ -1,14 +1,16 @@
 package bni
 
 const (
-	InHouseInquiryRequest = "IN_HOUSE_INQUIRY_REQUEST"
-	InHouseInquiryResponse = "IN_HOUSE_INQUIRY_RESPONSE"
-	InterBankInquiryRequest = "INTER_BANK_INQUIRY_REQUEST"
-	InterBankInquiryResponse = "INTER_BANK_INQUIRY_RESPONSE"
-	FeeTransferRequest = "FEE_TRANSFER_REQUEST"
-	FeeTransferResponse = "FEE_TRANSFER_RESPONSE"
-	InHouseTransferRequest = "IN_HOUSE_TRANSFER_REQUEST"
-	InHouseTransferResponse = "IN_HOUSE_TRANSFER_RESPONSE"
-	InterBankTransferRequest = "INTER_BANK_TRANSFER_REQUEST"
+	BalanceRequest            = "BALANCE_REQUEST"
+	BalanceResponse           = "BALANCE_RESPONSE"
+	InHouseInquiryRequest     = "IN_HOUSE_INQUIRY_REQUEST"
+	InHouseInquiryResponse    = "IN_HOUSE_INQUIRY_RESPONSE"
+	InterBankInquiryRequest   = "INTER_BANK_INQUIRY_REQUEST"
+	InterBankInquiryResponse  = "INTER_BANK_INQUIRY_RESPONSE"
+	FeeTransferRequest        = "FEE_TRANSFER_REQUEST"
+	FeeTransferResponse       = "FEE_TRANSFER_RESPONSE"
+	InHouseTransferRequest    = "IN_HOUSE_TRANSFER_REQUEST"
+	InHouseTransferResponse   = "IN_HOUSE_TRANSFER_RESPONSE"
+	InterBankTransferRequest  = "INTER_BANK_TRANSFER_REQUEST"
 	InterBankTransferResponse = "INTER_BANK_TRANSFER_RESPONSE"
 )
