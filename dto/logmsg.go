168469464e554eb4ebcde7502e75c47543fa4f42
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-07 06:22:28 +0700

    LogMsg: struct for logging

---
+++ dto/logmsg.go
@@ -0,0 +1,41 @@
+package dto
+
+import "encoding/json"
+
+type LogMsg struct {
+	Operation string `json:"OPERATION,omitempty"`
+	From      string `json:"FROM,omitempty"`
+	To        string `json:"TO,omitempty"`
+	RC        string `json:"RC,omitempty"`
+	CRN       string `json:"CRN,omitempty"`
+	RawMsg    string `json:"RAW_MSG,omitempty"`
+}
+
+func BuildLogRequest(operation, rc, crn string, dtoReq interface{}) LogMsg {
+	jsonMsg, err := json.Marshal(dtoReq)
+	rawMsg := string(jsonMsg)
+	if err != nil {
+		rawMsg = err.Error()
+	}
+
+	return LogMsg{
+		Operation: operation,
+		From:      "BNI", To: "API",
+		RC: rc, CRN: crn,
+		RawMsg: rawMsg,
+	}
+}
+func BuildLogResponse(operation, rc, crn string, dtoResp interface{}) LogMsg {
+	jsonMsg, err := json.Marshal(dtoResp)
+	rawMsg := string(jsonMsg)
+	if err != nil {
+		rawMsg = err.Error()
+	}
+
+	return LogMsg{
+		Operation: operation,
+		From:      "API", To: "BNI",
+		RC: rc, CRN: crn,
+		RawMsg: rawMsg,
+	}
+}
