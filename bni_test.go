d273100b29e3be0434c82b3ba8a9daf050732149
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-11-10 22:51:49 +0700

    GetBalance: integration test

--- bni_test.go
+++ bni_test.go
@@ -104,6 +104,163 @@ func TestBNI_DoAuthentication(t *testing.T) {
 	})
 }
 
+func TestBNI_GetBalance(t *testing.T) {
+	t.Run("good case", func(t *testing.T) {
+		givenConfig := config.Config{
+			AuthPath:        "/oauth",
+			BalancePath:     "/H2H/getbalance",
+			Username:        "dummyusername",
+			Password:        "dummypassword",
+			LogPath:         testLogPath,
+			SignatureConfig: dummySignatureConfig,
+		}
+
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			assert.Equal(t, http.MethodPost, req.Method)
+			assert.Equal(t, givenConfig.BalancePath, req.URL.Path)
+
+			assert.Equal(t, "application/json", req.Header.Get("content-type"))
+
+			var dtoResp dto.ApiResponse
+			getJSON("testdata/get_balance_response.json", &dtoResp)
+
+			w.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(w).Encode(dtoResp)
+			util.AssertErrNil(t, err)
+
+		}))
+		defer testServer.Close()
+
+		givenConfig.BNIServer = testServer.URL
+
+		bni := New(givenConfig)
+		bni.api.retryablehttpClient.HTTPClient = testServer.Client()
+
+		dtoReq := dto.GetBalanceRequest{AccountNo: "115471119"}
+
+		ctx := bniCtx.WithHTTPReqID(context.Background(), shortuuid.New())
+		dtoResp, err := bni.GetBalance(ctx, &dtoReq)
+
+		util.AssertErrNil(t, err)
+		assert.NotEmpty(t, dtoResp)
+	})
+
+	t.Run("bad response", func(t *testing.T) {
+		givenConfig := config.Config{
+			AuthPath:        "/oauth",
+			BalancePath:     "/H2H/getbalance",
+			Username:        "dummyusername",
+			Password:        "dummypassword",
+			LogPath:         testLogPath,
+			SignatureConfig: dummySignatureConfig,
+		}
+
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			assert.Equal(t, http.MethodPost, req.Method)
+			assert.Equal(t, givenConfig.BalancePath, req.URL.Path)
+
+			assert.Equal(t, "application/json", req.Header.Get("content-type"))
+
+			var dtoResp dto.ApiResponse
+			getJSON("testdata/bad_response.json", &dtoResp)
+
+			w.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(w).Encode(dtoResp)
+			util.AssertErrNil(t, err)
+
+		}))
+		defer testServer.Close()
+
+		givenConfig.BNIServer = testServer.URL
+
+		bni := New(givenConfig)
+		bni.api.retryablehttpClient.HTTPClient = testServer.Client()
+
+		dtoReq := dto.GetBalanceRequest{AccountNo: "115471119"}
+
+		ctx := bniCtx.WithHTTPReqID(context.Background(), shortuuid.New())
+		dtoResp, err := bni.GetBalance(ctx, &dtoReq)
+
+		util.AssertErrNotNil(t, err)
+		assert.Empty(t, dtoResp)
+	})
+
+	t.Run("no auth then good response", func(t *testing.T) {
+		givenConfig := config.Config{
+			AuthPath:        "/oauth",
+			BalancePath:     "/H2H/getbalance",
+			Username:        "dummyusername",
+			Password:        "dummypassword",
+			LogPath:         testLogPath,
+			SignatureConfig: dummySignatureConfig,
+		}
+
+		var hits uint64
+
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			assert.Equal(t, http.MethodPost, req.Method)
+
+			if hits == 0 {
+				assert.Equal(t, givenConfig.BalancePath, req.URL.Path)
+				w.WriteHeader(http.StatusUnauthorized)
+				hits++
+				return
+			}
+
+			if hits == 1 {
+				assert.Equal(t, givenConfig.AuthPath, req.URL.Path)
+
+				var dtoResp dto.GetTokenResponse
+				getJSON("testdata/get_token_response.json", &dtoResp)
+
+				w.WriteHeader(http.StatusOK)
+				err := json.NewEncoder(w).Encode(dtoResp)
+				util.AssertErrNil(t, err)
+
+				hits++
+				return
+			}
+
+			if hits == 2 {
+				var dtoResp dto.ApiResponse
+				getJSON("testdata/get_balance_response.json", &dtoResp)
+
+				w.WriteHeader(http.StatusOK)
+				err := json.NewEncoder(w).Encode(dtoResp)
+				util.AssertErrNil(t, err)
+				hits = 0
+			}
+
+		}))
+		defer testServer.Close()
+
+		givenConfig.BNIServer = testServer.URL
+
+		bni := New(givenConfig)
+		bni.api.retryablehttpClient.HTTPClient = testServer.Client()
+
+		dtoReq := dto.GetBalanceRequest{AccountNo: "115471119"}
+
+		firstReqID := shortuuid.New()
+		ctx := bniCtx.WithHTTPReqID(context.Background(), firstReqID)
+		dtoResp, err := bni.GetBalance(ctx, &dtoReq)
+
+		util.AssertErrNil(t, err)
+		assert.NotEmpty(t, dtoResp)
+
+		dtoReq = dto.GetBalanceRequest{AccountNo: "225471120"}
+
+		secondReqID := shortuuid.New()
+		ctx = bniCtx.WithHTTPReqID(context.Background(), secondReqID)
+		dtoResp, err = bni.GetBalance(ctx, &dtoReq)
+
+		util.AssertErrNil(t, err)
+		assert.NotEmpty(t, dtoResp)
+
+		t.Logf("firstReq: %s secondReq: %s", firstReqID, secondReqID)
+	})
+}
+
 func basicAuth(username, password string) string {
 	auth := username + ":" + password
 	return base64.StdEncoding.EncodeToString([]byte(auth))
