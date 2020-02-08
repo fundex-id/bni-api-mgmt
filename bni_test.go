ea4c9de9b5ec4b0339bd451393590cea41d466c2
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-02-08 14:02:29 +0700

    TestBNI_GetBalance: assert that access_token not empty in second retry attempt

--- bni_test.go
+++ bni_test.go
@@ -221,6 +221,8 @@ func TestBNI_GetBalance(t *testing.T) {
 			}
 
 			if hits == 2 {
+				assert.NotEmpty(t, req.URL.Query().Get("access_token"))
+
 				respByte := getJSON("testdata/get_balance_response.json")
 
 				w.WriteHeader(http.StatusOK)
